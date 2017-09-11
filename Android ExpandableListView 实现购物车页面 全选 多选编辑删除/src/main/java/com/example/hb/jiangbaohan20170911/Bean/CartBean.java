da3e4499764306aa45928a2f71ac85f11e951537
JiangBaohan <498805449@qq.com>
2017-09-11 10:03:49 +0800

    Android ExpandableListView 实现购物车页面 全选 多选编辑删除
     Android ExpandableListView 实现购物车

---
+++ Android ExpandableListView 实现购物车页面 全选 多选编辑删除/src/main/java/com/example/hb/jiangbaohan20170911/Bean/CartBean.java
@@ -0,0 +1,14 @@
+package com.example.hb.jiangbaohan20170911.Bean;
+
+/**
+ * data:2017/9/11
+ * author:汉堡(Administrator)
+ * function:
+ */
+
+public class CartBean {
+    /** 商品ID */
+    private String goodsid;
+    /** 店铺ID */
+    private String storeid;
+}
